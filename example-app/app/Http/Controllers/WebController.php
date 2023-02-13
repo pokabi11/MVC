87db6edbca1ad05775e1cbe3212fb3de002b439a
pokabi <hlong2k1@gmail.com>
2023-02-14 02:20:19 +0700

    test

---
+++ example-app/app/Http/Controllers/WebController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function home(){
+        return view("welcome");
+    }
+    public function aboutUs(){
+        return view("about-us");
+    }
+}
